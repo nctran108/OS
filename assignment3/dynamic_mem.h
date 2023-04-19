eefd55baa92ff9174a66e17518724360f5b5862b
Nguyen <nctran@vcu.edu>
2023-04-19 16:36:27 -0400

    new commit

--- assignment3/dynamic_mem.h
+++ assignment3/dynamic_mem.h
@@ -18,6 +18,7 @@ void *global_base = NULL;
 struct block_meta *find_free_block(struct block_meta **last, size_t size);
 struct block_meta *request_space(struct block_meta* last, size_t size);
 void *malloc(size_t size);
-void free(void *prt);
+struct block_meta *get_block_ptr(void *ptr);
+void free(void *ptr);
 
 #endif
