12450023f49e698dea22bd9c6244f0eaea4bfa1e
Nguyen <nctran@vcu.edu>
2023-04-20 03:40:05 -0400

    new commit

--- assignment3/main.c
+++ assignment3/main.c
@@ -1,6 +1,17 @@
+#include <stdio.h>
 #include "dynamic_mem.h"
 
 int main() {
+    int *arr = malloc(sizeof(int) * 10);
+
+    for(int i = 0; i < 10; i++){
+        arr[i] = i;
+    }
+
+    for(int i = 0; i < 10; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 
     return 0;
 }
